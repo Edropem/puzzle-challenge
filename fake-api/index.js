a8f1665b32f52ba3752e4469e354339a9ab63f25
Pedro <pedro.maresc@gmail.com>
2021-08-28 19:15:59 -0300

    Added example Gif, changed fake api call urls

--- fake-api/index.js
+++ fake-api/index.js
@@ -1,12 +1,11 @@
 const apiCall = [
     {title: "First Block",
-    image:['https://img2.rtve.es/i/?w=1600&i=1620920852564.jpg']},
+    image:['https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80']},
     {title: "Second Block",
-    image:['https://www.nacionrex.com/__export/1523478000646/sites/debate/img/2018/04/11/cc_2612273_167eb6e50b83410c8b1e00957abd1714_meme_otros_quien_demonios_la_entiende_del_todo_thumb_fb_1.jpg_242310155.jpg']},
+    image:['https://www.nativeplanet.com/img/2018/08/landscapebeautiful-1533110307.jpg']},
     {title: "Third Block",
-    image:[
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIL6hgQvlSTM4O0U3lETZFMiibWZpl-86xSQ&usqp=CAU']},
+    image:['https://visualwilderness.com/wp-content/uploads/2013/11/canada_3081.jpg']},
     {title: "Fourth Block",
-    image:['https://assets.entrepreneur.com/content/3x2/2000/20180706133830-meme.jpeg?width=700&crop=2:1']},
+    image:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCmJVNqiE7Lh7hYCKWh1JqCEt21VppR0MKMJSfow01dOS79D3A5Q1dxJpmCLz9tLjwm4Y&usqp=CAU']},
 ]
 export default apiCall
